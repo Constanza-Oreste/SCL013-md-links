4c16a5e07d4f2ffce467251417c910666efb17bc
Constanza-Oreste <constanzaorestesalazar@gmail.com>
2020-07-28 11:11:16 -0400

    Finding  md files from a path given by the user

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
-module.exports = () => {
-  // ...
-};
+
+/*module.exports = () => {
+
+}; */
+const fs = require('fs');
+
+
+
+// Node.js program to demonstrate the 
+// fs.readdir() method 
+  
+// Import the filesystem module 
+//const path = require('path'); 
+  
+// Function to get current filenames 
+// in directory with specific extension 
+
+/*fs.readdir(path, (err, files) => { 
+  if (err) 
+    console.log(err); 
+  else { 
+    console.log("\Filenames with the .md extension:"); 
+    files.forEach(file => { 
+      if (path.extname(file) == ".md") 
+        console.log(file); 
+    }) 
+  } 
+}) */
+ /*fs.readdir(path, (err,files) => {
+     if(err){
+         console.log(err);
+     }else{
+         console.log("\Filenames with the .md extension found:");
+         files.forEach(file=>{  
+            if (path.extname(file) == ".md")
+            console.log(file);
+         })
+     }
+ })*/
+ const path = process.argv[2];
+
+fs.readdir(path, function(err, items) {
+    console.log(items);
+ 
+    for (var i=0; i<items.length; i++) {
+        console.log(items[i]);
+    }
+});
