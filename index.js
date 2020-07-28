5346738bdd4263fed050cea7d4dbb674443a2f58
Constanza-Oreste <constanzaorestesalazar@gmail.com>
2020-07-28 12:52:39 -0400

    Reading content of a file

--- index.js
+++ index.js
@@ -3,14 +3,28 @@
 
 }; */
 const fs = require('fs');
+const path=require('path');
 
+fs.readdir( __dirname, (err, files) => { 
+  if (err) 
+    console.log(err); 
+  else { 
+    console.log("\Filenames with the .md extension:"); 
+    files.forEach(file => { 
+      if (path.extname(file) == ".md") 
+        console.log(file); 
+    }) 
+  } 
+})
 
+fs.readFile('README.md', 'utf8', function(err, data){ 
+      
+  // Display the file content 
+    console.log(data); 
+}); 
 
-// Node.js program to demonstrate the 
-// fs.readdir() method 
   
-// Import the filesystem module 
-//const path = require('path'); 
+
   
 // Function to get current filenames 
 // in directory with specific extension 
@@ -37,22 +51,12 @@ const fs = require('fs');
          })
      }
  })*/
-const path = process.argv[2];
+/*const path = process.argv[2];
 fs.readdir(path, function(err, items) {
     console.log(items);
  
     for (var i=0; i<items.length; i++) {
         console.log(items[i]);
     }
-});
-/*fs.readdir( __dirname, (err, files) => { 
-    if (err) 
-      console.log(err); 
-    else { 
-      console.log("\Filenames with the .md extension:"); 
-      files.forEach(file => { 
-        if (path.extname(file) == ".md") 
-          console.log(file); 
-      }) 
-    } 
-  })*/
+});*/ 
+
