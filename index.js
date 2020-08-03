2090ea305d6e4422b2a3b375b9ade64b5e7bb386
Constanza-Oreste <constanzaorestesalazar@gmail.com>
2020-08-03 13:32:15 -0400

    Buscando solo los links en el documento md

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
-
-/*module.exports = () => {
-
-}; */
-const fs = require('fs');
-//const path=require('path');
-const path = process.argv[2]; //toma el nombre del archivo
- console.log(process.argv[0], "esta funcionando el 0");
- console.log(process.argv[1], "funciona el 1");
- console.log(process.argv[2], "ARCHIVO MD NO ENCONTRADO");
-
-
- fs.readFile(path,'utf8', function (err, data) {
-  console.log("leyendo archivo");
-if(err){
-  console.log(`Error ${err}`);
-
-}else {
-  console.log(data.trim().split('\n').filter(word => word.includes('https://')));
-  
-}
+const fs = require ('fs');  //ve los archivos
+const Marked = require ('marked'); // transforma markdown a HTML
+//const fetch = require('fetch');
+const pathN = require('path')  // ve las rutas 
+const process = require('process');
+const chalk = require('chalk'); // da colores 
+let path = process.argv[2]
+path = pathN.resolve(path);
+console.log("CAMBIAR mi ruta es POR OTRA COSA" + path)
+//lee archivo md
+const reaArcMd = (path => {
+  return new Promise(( resolve,reject) =>{
+    fs.readFile(path,'utf8', (err,data)=>{
+      if (err){
+        reject(console.log("CAMBIAR ESTO POR ASCII" + path))
+      }
+      resolve(data)
+      //console.log(data);
+    })
+  })
 });
-
+const filtLinks = (links) => {
+  return links.filter((link) => {
+      const prefix = link.href.substring(0, 4); // averiguar porque es 0,4 porque si pongo otro numero no pasa
+      if (prefix == 'http') {
+          return true;
+      } else {
+          return false;
+      }
+  })
+}
+//obtiene links del archivo md
+const getLinks = (path) => {
+    return new Promise ((resolve,reject)=>{ //averiguar resolve y reject
+      reaArcMd(path).then(res =>{
+      let links = [];
+      const renderer = new Marked.Renderer(); // averiguar rendered y 
+      renderer.link = function(href, title, text){
+          links.push({
+            //
+            href:href,
+            //
+            text:text,
+            //
+            path: path})
+        }
+        Marked(res,{renderer:renderer})
+        resolve(links)
+        links = filtLinks(links);
+        links.map(element =>{console.log(`
+${chalk.yellow("href: "+element.href)}
+${chalk.green("text :"+element.text)}
+${chalk.red("path :"+element.path)}
+        `)});
+          })
+          .catch(err =>{
+            reject(err)
+          })
+        })
+    }
+    getLinks(path)
 
 
 
