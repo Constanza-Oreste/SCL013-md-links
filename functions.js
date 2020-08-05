f85e53e9a43fe3015c5aa911583c6da5ddfd1967
Constanza-Oreste <constanzaorestesalazar@gmail.com>
2020-08-05 19:02:52 -0400

    intentando usar fetch

--- functions.js
+++ functions.js
@@ -7,3 +7,13 @@ urlExists('https://www.google.com', function(err, exists) {
 /*urlExists('https://www.fakeurl.notreal', function(err, exists) {
   console.log(exists); // false 
 });*/
+const fetch=require('node-fetch');
+
+
+
+let linksResponse=fetch('https://github.com/');
+linksResponse.then((res)=>{
+  return res.json();
+}).then((json)=>{
+  console.log(json);
+}).catch(err => console.error(err));
--- index.js
+++ index.js
@@ -63,15 +63,22 @@ fs.readdir(path, function(err, items) {
 });*/ 
 const fs = require ('fs');  //ve los archivos
 const Marked = require ('marked'); // transforma markdown a HTML
-//const fetch = require('fetch');
 const pathN = require('path')  // ve las rutas 
 const process = require('process');
 const chalk = require('chalk'); // da colores 
 const { argv } = require('process');
+const fetch=require('node-fetch');
+
 let path = process.argv[2]
 let option = process.argv[3]
 path = pathN.resolve(path);
-//console.log("CAMBIAR mi ruta es POR OTRA COSA" + path)
+
+let linksResponse=fetch('https://www.google.cl');
+linksResponse.then((res)=>{
+  return res.json();
+}).then((json)=>{
+  console.log(json);
+})
 let stats = false;
 let validate = false;
 if (process.argv.includes('--stats' && '--validate')) {
@@ -112,14 +119,13 @@ const readPath = (path => {
         reject(console.log("CAMBIAR ESTO POR ASCII" + path))
       }
       resolve(data)
-      //console.log(data);
     })
   })
 });
 const filterLinks = (links) => {
   return links.filter((link) => {
-      const prefix = link.href.substring(0, 4); // averiguar porque es 0,4 porque si pongo otro numero no pasa
-      if (prefix == 'http') {
+      const http = link.href.substring(0, 4); // Extrae caracteres desde posicion 0 a 4
+      if (http == 'http') {
           return true;
       } else {
           return false;
@@ -144,11 +150,11 @@ const getLinks = (path) => {
         Marked(res,{renderer:renderer})
         resolve(links)
         links = filterLinks(links);
-       /* links.map(element =>{console.log("hola"`
+       links.map(element =>{console.log("hola"`
 ${chalk.yellow("href: "+element.href)}
 ${chalk.green("text :"+element.text)}
 ${chalk.cyan("path :"+element.path)}
-        `)});*/
+        `)});
           })
           .catch(err =>{
             reject(err)
